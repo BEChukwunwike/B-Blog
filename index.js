607698bfcf58f479872a74b1ffb3b3d743bdb324
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2024-01-23 12:53:00 +0100

    Update dependencies and fix styling in header.css

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.post("/create-post" (req, res) =>{
+    const title = req.body.title;
+    const content = req.body.content;
+      
+    // ... your code for processing and storing the post data ...
+})
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong! Please try again later.');
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
